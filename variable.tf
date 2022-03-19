f77f36ea865023d030ef1a08d47ef7ed5e4d68e4
naushadarchitect <101919314+naushadarchitect@users.noreply.github.com>
2022-03-19 18:40:54 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+
+variable "list" {
+  type = list
+  default = ["m5.large","m5.xlarge","t2.medium"]
+}
+
+variable "types" {
+  type = map
+  default = {
+    us-east-1 = "t2.micro"
+    us-west-2 = "t2.nano"
+    ap-south-1 = "t2.small"
+  }
+}
